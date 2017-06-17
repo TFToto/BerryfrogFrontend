9b72cc0b33c671ce76da3ee5a85f7e0fe46d07c4
Torsten Freyda <torsten@freyda.de>
2017-06-17 09:39:51 +0200

    Extension of the data presentation - add the last 7 days - add the last 30 days - add average values

--- src/Berryfrog/ApiBundle/Controller/RestController.php
+++ src/Berryfrog/ApiBundle/Controller/RestController.php
@@ -19,13 +19,19 @@ class RestController extends Controller {
 			case 'currentvalues':
 				$return = $this->_getCurrentValues();
 				break;
-			case 'datetimerange';
+			case 'datetimerange':
 				$return = $this->_getRangeValues();
 				break;
-			case 'last24hours';
+			case 'last24hours':
 				$return = $this->_getlast24Hours();
 				break;
-			case 'sensors';
+			case 'last7days':
+				$return = $this->_getlastXDays(7);
+				break;
+			case 'last30days':
+				$return = $this->_getlastXDays(30);
+				break;
+			case 'sensors':
 				$return = $this->_getSensorIds();
 				break;
 			default:
@@ -65,6 +71,100 @@ class RestController extends Controller {
 	
 		return $db->getQuery()->getResult();
 	
+	}
+	
+	/**
+	 * Method for to get the last 7 days of values from sensor
+	 *
+	 * @return array one db object result
+	 */
+	private function _getlastXDays($days) {
+		
+		//$logger = $this->get('logger');
+		//$logger->info('We just go the logger');
+		
+		$result = array();
+		$day_i = $days;
+
+		for ($i = 1; $i <= $days; $i++) { 
+			
+			$db = $this->_repository->createQueryBuilder('r');
+			$db->select('
+				min(r.tempDhtHic) as mintemp_dht_hic,
+				max(r.tempDhtHic) as maxtemp_dht_hic,
+				avg(r.tempDhtHic) as avgtemp_dht_hic,
+				min(r.tempDhtHif) as mintemp_dht_hif,
+				max(r.tempDhtHif) as maxtemp_dht_hif,
+				avg(r.tempDhtHif) as avgtemp_dht_hif,
+				min(r.humidityDht) as minhumidity_dht,
+				max(r.humidityDht) as maxhumidity_dht,
+				avg(r.humidityDht) as avghumidity_dht,
+				min(r.pressureBmp) as minpressure_bmp,
+				max(r.pressureBmp) as maxpressure_bmp,
+				avg(r.pressureBmp) as avgpressure_bmp,
+				min(r.tempBmp) as mintemp_bmp,
+				max(r.tempBmp) as maxtemp_bmp,
+				avg(r.tempBmp) as avgtemp_bmp,
+				min(r.addDatetime) as mindatetime,
+				max(r.addDatetime) as maxdatetime
+			');
+
+			$greater_date = date('Y-m-d 00:00:00', strtotime('-'.$day_i.' day'));
+			$less_date = date('Y-m-d 23:59:59', strtotime('-'.$day_i.' day'));
+				
+			$db->setParameter('greaterdate', $greater_date);
+			$db->setParameter('lessdate', $less_date);
+			$db->where('r.addDatetime < :lessdate AND r.addDatetime > :greaterdate');
+			
+			$db->orderBy('r.addDatetime','asc');
+			
+			//$logger->debug('datas'.print_r($db->getQuery()->getResult(),true));
+			
+			$data = $db->getQuery()->getResult();
+			
+			if($data[0]['maxtemp_dht_hic'] > 60 || $data[0]['maxtemp_bmp'] > 60) {
+				//continue;
+			}
+			
+			if($data[0]['mintemp_dht_hic'] < -50 || $data[0]['mintemp_bmp'] < -50) {
+				continue;
+			}
+			$datas = array(
+				'mintemp_dht_hic' => number_format($data[0]['mintemp_dht_hic'],2),
+				'maxtemp_dht_hic' => number_format($data[0]['maxtemp_dht_hic'],2),
+				'avgtemp_dht_hic' => number_format($data[0]['avgtemp_dht_hic'],2),
+				'mintemp_dht_hif' => number_format($data[0]['mintemp_dht_hif'],2),
+				'maxtemp_dht_hif' => number_format($data[0]['maxtemp_dht_hif'],2),
+				'avgtemp_dht_hif' => number_format($data[0]['avgtemp_dht_hif'],2),
+				'minhumidity_dht' => number_format($data[0]['minhumidity_dht'],2),
+				'maxhumidity_dht' => number_format($data[0]['maxhumidity_dht'],2),
+				'avghumidity_dht' => number_format($data[0]['avghumidity_dht'],2),
+				'minpressure_bmp' => number_format($data[0]['minpressure_bmp'],2),
+				'maxpressure_bmp' => number_format($data[0]['maxpressure_bmp'],2),
+				'avgpressure_bmp' => number_format($data[0]['avgpressure_bmp'],2),
+				'mintemp_bmp' => number_format($data[0]['mintemp_bmp'],2),
+				'maxtemp_bmp' => number_format($data[0]['maxtemp_bmp'],2),
+				'avgtemp_bmp' => number_format($data[0]['avgtemp_bmp'],2),
+				'datetime_from' => $data[0]['mindatetime'],
+				'datetime_to' => $data[0]['maxdatetime'],
+				'dateday' => date("d.m.", strtotime($data[0]['mindatetime']))
+			);
+
+			//Return only valid temperatures in range of -50 to +60 °C
+			if(
+				number_format($data[0]['maxtemp_dht_hic'],2) < 60 && 
+				number_format($data[0]['maxtemp_bmp'],2) < 60 && 
+				number_format($data[0]['maxtemp_dht_hic'],2) > -50 && 
+				number_format($data[0]['maxtemp_bmp'],2) > -50
+			) {
+				array_push($result,$datas);
+			}
+
+			$day_i = $day_i -1;
+
+		}
+
+		return $result;
 	}
 	
 	/**
