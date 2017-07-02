5b8986fccc97eb60989b1a3118f606647fbf7d51
Torsten Freyda <torsten@freyda.de>
2017-07-02 17:37:49 +0200

    Add Static tab

--- src/Berryfrog/ApiBundle/Controller/RestController.php
+++ src/Berryfrog/ApiBundle/Controller/RestController.php
@@ -10,6 +10,15 @@ class RestController extends Controller {
 	 * db object
 	 */
 	private $_repository;
+	
+	private $_DEFSUMMERDAY_START = '>= 25';
+	private $_DEFSUMMERDAY_END = '> 30';
+	private $_DEFHOTDAY_START = '>= 30';
+	private $_DEFHOTDAY_END = '> 35';
+	private $_DEFDESERTDAY_START = '>= 35';
+	private $_DEFDESERTDAY_END = '> 60';
+	private $_TROPICALNIGHT_START = '>=20';
+	private $_TROPICALNIGHT_END = '> 40';
 
 	public function getMeasurementAction($type) {
 		
@@ -33,6 +42,9 @@ class RestController extends Controller {
 				break;
 			case 'sensors':
 				$return = $this->_getSensorIds();
+				break;
+			case 'static':
+				$return = $this->_getStatic();
 				break;
 			default:
 				$return = null;
@@ -127,7 +139,7 @@ class RestController extends Controller {
 			}
 			
 			if($data[0]['mintemp_dht_hic'] < -50 || $data[0]['mintemp_bmp'] < -50) {
-				continue;
+				//continue;
 			}
 			$datas = array(
 				'mintemp_dht_hic' => number_format($data[0]['mintemp_dht_hic'],2),
@@ -168,6 +180,49 @@ class RestController extends Controller {
 	}
 	
 	/**
+	 * Method for to get the last 7 days of values from sensor
+	 *
+	 * @return array one db object result
+	 */
+	private function _getAVGOfMonth($date) {
+	    
+	    $result = array();
+	        
+        $db = $this->_repository->createQueryBuilder('r');
+        $db->select('
+			min(r.tempDhtHic) as mintemp_dht_hic,
+			max(r.tempDhtHic) as maxtemp_dht_hic,
+			avg(r.tempDhtHic) as avgtemp_dht_hic,
+			min(r.tempDhtHif) as mintemp_dht_hif,
+			max(r.tempDhtHif) as maxtemp_dht_hif,
+			avg(r.tempDhtHif) as avgtemp_dht_hif,
+			min(r.tempBmp) as mintemp_bmp,
+			max(r.tempBmp) as maxtemp_bmp,
+			avg(r.tempBmp) as avgtemp_bmp,
+			min(r.addDatetime) as mindatetime,
+			max(r.addDatetime) as maxdatetime
+		');
+        
+        $greater_date = date($date.'-01 00:00:00');
+        $less_date = date($date.'-31 23:59:59');
+        
+        $db->setParameter('greaterdate', $greater_date);
+        $db->setParameter('lessdate', $less_date);
+        $db->where('
+            r.addDatetime < :lessdate AND 
+            r.addDatetime > :greaterdate AND
+            r.tempDhtHic < 60 AND
+            r.tempBmp < 60 AND
+            r.tempDhtHic > -50 AND
+            r.tempBmp > -50
+        ');
+
+        $data = $db->getQuery()->getResult();
+
+        return ($data[0]['avgtemp_dht_hic'] + $data[0]['avgtemp_bmp']) / 2;
+	}
+	
+	/**
 	 * Method for to get a range of values from sensor
 	 *
 	 * @return array one db object result
@@ -195,5 +250,224 @@ class RestController extends Controller {
 		
 		return $db->getQuery()->getResult();
 	
+	}
+	/**
+	 * Method for to get weather sttic values
+	 * @param string $order oder direction asc or desc	 * 
+	 * @return array
+	 */
+	private function _getAvailableYears($oder='asc') {
+		
+		$db = $this->_repository
+			->createQueryBuilder('r')
+			->select('date_format(r.addDatetime,\'%Y\') AS year')
+			->orderBy('year',$oder)
+			->groupBy('year');
+		
+		
+		return $db->getQuery()->getResult();
+	}
+	/**
+	 * Method for to get weather sttic values
+	 * @param string $order oder direction asc or desc	 *
+	 * @return array
+	 */
+	private function _getAvailableMonths($year,$oder='asc') {
+
+		if(is_numeric($year)) {
+			$greater_date = date($year.'-01-01 00:00:00');
+			$less_date = date($year.'-12-31 23:59:59');
+
+			$db = $this->_repository
+				->createQueryBuilder('r')
+				->select('date_format(r.addDatetime,\'%m\') AS month')
+				->setParameter('greaterdate', $greater_date)
+				->setParameter('lessdate', $less_date)
+				->where('r.addDatetime < :lessdate AND r.addDatetime > :greaterdate')
+				->orderBy('month',$oder)
+				->groupBy('month');
+
+			return $db->getQuery()->getResult();
+		} else {
+			return null;
+		}
+	}
+	/**
+	 * Method for to get weather sttic values
+	 * @param string $order oder direction asc or desc	 *
+	 * @return array
+	 */
+	private function _getAvailableDaysPerMonth($date,$oder='asc') {
+	    
+
+        $greater_date = date($date.'-01 00:00:00');
+        $less_date = date($date.'-31 23:59:59');
+        
+        $db = $this->_repository
+            ->createQueryBuilder('r')
+            ->select('date_format(r.addDatetime,\'%d\') AS day')
+            ->setParameter('greaterdate', $greater_date)
+            ->setParameter('lessdate', $less_date)
+            ->where('r.addDatetime < :lessdate AND r.addDatetime > :greaterdate')
+            ->orderBy('day',$oder)
+            ->groupBy('day');
+        
+        return $db->getQuery()->getResult();
+
+	}
+
+	/**
+	 * Method for to get weather sttic values
+	 *
+	 * @return array
+	 */
+	private function _getStatic() {
+		
+		$results = array();
+
+		foreach ($this->_getAvailableYears() as $year) {
+
+			$months = $this->_getAvailableMonths($year['year']);
+			$month_results = array();
+			
+			foreach ($months as $month) {
+			    
+			    $monthvalues = array($month['month'] => $this->_getMonthStatic($year['year'].'-'.$month['month']));
+				array_push($month_results, $monthvalues);
+			}
+			
+			$datas = array($year['year']=> $month_results);
+			array_push($results,$datas);
+		}
+		return $results;
+	}
+	
+	/**
+	 * Method for to get static vaules per months
+	 * 
+	 * @param string $date example: '12-2017' (MM-YYYY)
+	 * @return array
+	 */
+	private function _getMonthStatic($date) {
+		
+		$db = $this->_repository->createQueryBuilder('r');
+		$db->select('
+				min(r.tempDhtHic) as mintemp_dht_hic,
+				max(r.tempDhtHic) as maxtemp_dht_hic,
+				avg(r.tempDhtHic) as avgtemp_dht_hic,
+				min(r.tempDhtHif) as mintemp_dht_hif,
+				max(r.tempDhtHif) as maxtemp_dht_hif,
+				avg(r.tempDhtHif) as avgtemp_dht_hif,
+				min(r.humidityDht) as minhumidity_dht,
+				max(r.humidityDht) as maxhumidity_dht,
+				avg(r.humidityDht) as avghumidity_dht,
+				min(r.pressureBmp) as minpressure_bmp,
+				max(r.pressureBmp) as maxpressure_bmp,
+				avg(r.pressureBmp) as avgpressure_bmp,
+				min(r.tempBmp) as mintemp_bmp,
+				max(r.tempBmp) as maxtemp_bmp,
+				avg(r.tempBmp) as avgtemp_bmp
+			');
+		
+		$greater_date = date($date.'-01 00:00:00');
+		$less_date = date($date.'-31 23:59:59');
+		
+		$db->setParameter('greaterdate', $greater_date);
+		$db->setParameter('lessdate', $less_date);
+		$db->where('
+            r.addDatetime < :lessdate AND 
+            r.addDatetime > :greaterdate AND
+            r.tempDhtHic < 60 AND
+            r.tempBmp < 60 AND
+            r.tempDhtHic > -50 AND
+            r.tempBmp > -50
+        ');
+		
+		$db->orderBy('r.addDatetime','asc');
+		
+		$data = $db->getQuery()->getResult();
+		
+		$summerdays = $this->_getNumOfDayDefs($date,$this->_DEFSUMMERDAY_START,$this->_DEFSUMMERDAY_END);
+		$hotdays = $this->_getNumOfDayDefs($date,$this->_DEFHOTDAY_START,$this->_DEFHOTDAY_END);
+		$desertdays = $this->_getNumOfDayDefs($date,$this->_DEFDESERTDAY_START,$this->_DEFDESERTDAY_END);
+		$tropicalnights_part1 = $this->_getNumOfDayDefs($date,$this->_TROPICALNIGHT_START,$this->_TROPICALNIGHT_END,'22:00:00');
+		$tropicalnights_part2 = $this->_getNumOfDayDefs($date,$this->_TROPICALNIGHT_START,$this->_TROPICALNIGHT_END,'0:00:00','06:00:00');
+		
+		$datas = array(
+		    'mintemp_dht_hic' => number_format($data[0]['mintemp_dht_hic'],2),
+		    'maxtemp_dht_hic' => number_format($data[0]['maxtemp_dht_hic'],2),
+		    'avgtemp_dht_hic' => number_format($data[0]['avgtemp_dht_hic'],2),
+		    'mintemp_dht_hif' => number_format($data[0]['mintemp_dht_hif'],2),
+		    'maxtemp_dht_hif' => number_format($data[0]['maxtemp_dht_hif'],2),
+		    'avgtemp_dht_hif' => number_format($data[0]['avgtemp_dht_hif'],2),
+		    'minhumidity_dht' => number_format($data[0]['minhumidity_dht'],2),
+		    'maxhumidity_dht' => number_format($data[0]['maxhumidity_dht'],2),
+		    'avghumidity_dht' => number_format($data[0]['avghumidity_dht'],2),
+		    'minpressure_bmp' => number_format($data[0]['minpressure_bmp'],2),
+		    'maxpressure_bmp' => number_format($data[0]['maxpressure_bmp'],2),
+		    'avgpressure_bmp' => number_format($data[0]['avgpressure_bmp'],2),
+		    'mintemp_bmp' => number_format($data[0]['mintemp_bmp'],2),
+		    'maxtemp_bmp' => number_format($data[0]['maxtemp_bmp'],2),
+		    'avgtemp_bmp' => number_format($data[0]['avgtemp_bmp'],2),
+		    'summerdays' => $summerdays,
+		    'hotdays' => $hotdays,
+		    'desertdays' => $desertdays,
+		    'tropicalnights' => $tropicalnights_part1 + $tropicalnights_part2
+		);
+
+		return $datas;
+	}
+	
+	private function _getNumOfDayDefs($date,$start,$end) {
+
+	    $days = 0;
+	    $days_per_month = $this->_getAvailableDaysPerMonth($date);
+	    
+	    foreach ($days_per_month as $day) {
+	        $temps = $this->_getDayDefinition(
+	            $date.'-'.$day['day'],
+	            $start,
+	            $end
+	            );
+	        
+	        if($temps >= 1) {
+	            $days = $days +1;
+	        }
+	    }
+	    
+	    return $days;
+	}
+	
+	/**
+	 * Method for to get static vaules per months
+	 *
+	 * @param string $date example: '24-12-2017' (DD-MM-YYYY)
+	 * @return array
+	 */
+	private function _getDayDefinition($date,$range_definition_start,$range_definition_end,$time_start='00:00:00',$time_end='23:59:59') {
+	    
+	    $db = $this->_repository->createQueryBuilder('r');
+	    $db->select('count(r.id)');
+	    
+	    $greater_date = date($date.' '.$time_start);
+	    $less_date = date($date.' '.$time_end);
+	    
+	    $db->setParameter('greaterdate', $greater_date);
+	    $db->setParameter('lessdate', $less_date);
+	    $db->where('
+            r.tempBmp '.$range_definition_start.' AND
+            r.tempDhtHic '.$range_definition_start.' AND
+            r.tempBmp '.$range_definition_end.' AND
+            r.tempDhtHic '.$range_definition_end.' AND
+            r.addDatetime < :lessdate AND
+            r.addDatetime > :greaterdate AND
+            r.tempDhtHic < 60 AND
+            r.tempBmp < 60 AND
+            r.tempDhtHic > -50 AND
+            r.tempBmp > -50
+        ');
+	    
+
+	    return $db->getQuery()->getSingleScalarResult();
 	}
 }
